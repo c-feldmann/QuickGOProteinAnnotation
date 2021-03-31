2d4f4e6d1a97e6e5cdf4792df8d49a822982c1c5
cfeldmann <ueb.garnix@googlemail.com>
2021-03-31 17:15:56 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='QuckGOProteinAnnotation',
+    version='0.1',
+    author='Christian Feldmann',
+    license="BSD",
+    packages=['go_protein_annotation', ],
+    author_email='cfeldmann@bit.uni-bonn.de',
+    description='Classes and functions useful for chemoinformatics',
+    install_requires=['pandas', 'networkx', 'requests']
+)
